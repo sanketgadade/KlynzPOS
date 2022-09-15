74a5f09a1833e859234d987b15635d8791b6c089
SanketGadade <sanketgadade4@gmail.com>
2022-09-15 12:50:43 +0530

    addec url of prd API

--- src/AxiosAPI/APIConfig.js
+++ src/AxiosAPI/APIConfig.js
@@ -3,3 +3,6 @@ export const stageAPIURL = `http://localhost:3001/`;
 
 //URL for Local
 export const LocalAPIURL = `http://localhost:3001/`;
+
+//URL for PRD
+export const PrdAPIURL = `http://localhost:3001/`;
